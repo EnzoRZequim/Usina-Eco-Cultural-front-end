ce93a44dbd4f3d10f16a20daee3876edf9459d37
Thamires <thamiipie@gmail.com>
2024-10-18 16:34:55 -0300

    revert last commit

--- front/components/navbar.js
+++ front/components/navbar.js
@@ -11,20 +11,20 @@ class Navbar {
         <div class="navheader">
             <img src="svg/logo-usina.svg" alt="Logo" class="logo" />
             <div class="navbar">
-                <a href="./home.html" class="${
-                  currentPath.includes("./pages/home") ? "active" : ""
+                <a href="/home" class="${
+                  currentPath.includes("/home") ? "active" : ""
                 }">Início</a>
-                <a href="./eventos.html" class="${
-                  currentPath.includes("./pages/eventos") ? "active" : ""
+                <a href="/eventos" class="${
+                  currentPath.includes("/eventos") ? "active" : ""
                 }">Eventos</a>
-                <a href="./noticias.html" class="${
-                  currentPath.includes("./pages/noticias") ? "active" : ""
+                <a href="/noticias" class="${
+                  currentPath.includes("/noticias") ? "active" : ""
                 }">Notícias</a>
-                <a href="./loja.html" class="${
-                  currentPath.includes("./pages/loja") ? "active" : ""
+                <a href="/loja" class="${
+                  currentPath.includes("/loja") ? "active" : ""
                 }">Loja</a>
-                <a href="./contato.html" class="${
-                  currentPath.includes("./pages/contato") ? "active" : ""
+                <a href="/contato" class="${
+                  currentPath.includes("/contato") ? "active" : ""
                 }">Contato</a>
             </div>
             <a href="/login" class="login-link">Fazer Login</a>
