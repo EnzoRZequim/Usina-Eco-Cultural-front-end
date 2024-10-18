24a669b6f5d1e69aa8c6fb74d1d8a7110de7a47f
Thamires <thamiipie@gmail.com>
2024-10-18 17:21:56 -0300

    formatações arrumadas

--- index.js
+++ index.js
@@ -1,33 +1,37 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Serve arquivos estáticos da pasta "front"
-app.use(express.static(path.join(__dirname, 'front')));
+app.use(express.static(path.join(__dirname, "front")));
 
 // Rota para a página inicial
-app.get('/', (req, res) => {
-    res.redirect('/home'); // Redireciona para a página inicial
+app.get("/", (req, res) => {
+  res.redirect("/home"); // Redireciona para a página inicial
 });
 
 // Rota para a página inicial
-app.get('/home', (req, res) => {
-    res.sendFile(path.join(__dirname, 'front/pages/home.html'));
+app.get("/home", (req, res) => {
+  res.sendFile(path.join(__dirname, "front/pages/home.html"));
 });
 
 // Rota para a página de contato
-app.get('/contato', (req, res) => {
-    res.sendFile(path.join(__dirname, 'front/pages/contato.html'));
+app.get("/contato", (req, res) => {
+  res.sendFile(path.join(__dirname, "front/pages/contato.html"));
 });
 
 // Rota para a página de login
-app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, 'front/pages/login.html')); // Altere o caminho conforme necessário
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "front/pages/login.html")); // Altere o caminho conforme necessário
+});
+
+app.get("/contato", (req, res) => {
+  res.sendFile(path.join(__dirname, "front/pages/contato.html"));
 });
 
 // Inicie o servidor
 app.listen(PORT, () => {
-    console.log(`Servidor rodando em http://localhost:${PORT}`);
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
