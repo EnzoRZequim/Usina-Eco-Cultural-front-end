1787fdcfb0ab9ad6028de72de4e03e6d266b52de
Thamires <thamiipie@gmail.com>
2024-10-22 13:32:31 -0300

    fix other paths

--- front/components/navbar.js
+++ front/components/navbar.js
@@ -11,19 +11,19 @@ class Navbar {
         <div class="navheader">
             <img src="../svg/logo-usina.svg" alt="Logo" class="logo" />
             <div class="navbar">
-                <a href="/pages/home.html" class="${
+                <a href="../pages/home.html" class="${
                   currentPath.endsWith("/pages/home.html") ? "active" : ""
                 }">Início</a>
-                <a href="/eventos" class="${
-                  currentPath.startsWith("/eventos") ? "active" : ""
+                <a href="../pages/eventos.html" class="${
+                  currentPath.endsWith("/eventos") ? "active" : ""
                 }">Eventos</a>
-                <a href="/noticias" class="${
-                  currentPath.startsWith("/noticias") ? "active" : ""
+                <a href="../pages/noticias.html" class="${
+                  currentPath.endsWith("/noticias") ? "active" : ""
                 }">Notícias</a>
-                <a href="/loja" class="${
-                  currentPath.startsWith("/loja") ? "active" : ""
+                <a href="../pages/loja.html" class="${
+                  currentPath.endsWith("/loja") ? "active" : ""
                 }">Loja</a>
-                <a href="/pages/contato.html" class="${
+                <a href="../pages/contato.html" class="${
                   currentPath.endsWith("/pages/contato.html") ? "active" : ""
                 }">Contato</a>
             </div>
