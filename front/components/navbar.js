a925d965ebef4887427046c69ab877269124066e
Thamires <thamiipie@gmail.com>
2024-10-31 15:23:22 -0300

    add bootstrap navbar + mobile

--- front/components/navbar.js
+++ front/components/navbar.js
@@ -8,28 +8,68 @@ class Navbar {
     const currentPath = window.location.pathname;
 
     this.element.innerHTML = `
-    <div class="container">
-      <div class="row justify-content-center">
-        <div class="col-md-8">
-          <nav class="navbar navbar-expand-md">
-            <img src="../svg/logo-usina.svg" alt="Usina Eco Cultural" class="logo d-none d-md-block" /> <!-- Removendo margem -->
-            <div>
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#myNav" aria-controls="myNav" aria-expanded="false" aria-label="Toggle Navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="myNav">
-                <div class="d-lg-flex justify-content-between w-100">
-                  <div class="navbar-nav me-auto">
-                    <a class="nav-link active me-2" aria-current="page" href="./login.html">login</a>
-                    <a class="nav-link active me-2" data-bs-toggle="modal" data-bs-target="#modalCadastro" aria-current="page" href="#">Novo Usuario</a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </nav>
+    <div>
+      <button class="btn btn-lg d-block d-lg-none" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
+        <i class="fi fi-br-menu-burger" style="color: black; font-size: xx-large;"></i>
+      </button>
+         
+      
+      <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+        <div class="offcanvas-header d-flex align-items-center justify-content-between">
+          <h5 class="offcanvas-title flex-grow-1 text-center" id="offcanvasExampleLabel">
+            <img src="../svg/logo-usina-preta.svg" alt="Usina Eco Cultural" class="logo" />
+          </h5>
+          <button type="button" class="btn text-reset text-black fw-bolder" style="font-size: x-large; background: none; border: none; padding: 0;" data-bs-dismiss="offcanvas" aria-label="Close">
+            <i class="fi fi-br-cross"></i>
+          </button>
+          
+        </div>
+        
+        <div class="offcanvas-body align-content-between">
+          <ul class="list-unstyled p-10 fw-bolder" style="font-size: larger;">
+            <li class="mb-3">
+              <a class="text-decoration-none ${currentPath.endsWith('/pages/home.html') ? 'active' : ''}" href="../pages/home.html">Início</a>
+            </li>
+            <li class="mb-3">
+              <a class="text-decoration-none ${currentPath.endsWith('/pages/contato.html') ? 'active' : ''}" href="../pages/contato.html">Contato</a>
+            </li>
+            <li class="mb-3">
+              <a class="text-decoration-none ${currentPath.endsWith('/pages/eventos.html') ? 'active' : ''}" href="../pages/eventos.html">Eventos</a>
+            </li>
+            <li class="mb-3">
+              <a class="text-decoration-none ${currentPath.endsWith('/pages/noticias.html') ? 'active' : ''}" href="../pages/noticias.html">Notícias</a>
+            </li>
+            <li class="mb-3">
+              <a class="text-decoration-none ${currentPath.endsWith('/pages/loja.html') ? 'active' : ''}" href="../pages/loja.html">Loja</a>
+            </li>
+          </ul>
         </div>
       </div>
+      
+      
+      
+
+    <div class="container align-content-center d-none d-lg-block">
+      <div class="navbar"> <!-- Hides on small screens, shows on large -->
+        <a href="../pages/home.html" class="${
+          currentPath.endsWith("/pages/home.html") ? "active" : ""
+        }">Início</a>
+        <a href="../pages/eventos.html" class="${
+          currentPath.endsWith("/pages/eventos.html") ? "active" : ""
+        }">Eventos</a>
+        <a href="../pages/noticias.html" class="${
+          currentPath.endsWith("/pages/noticias.html") ? "active" : ""
+        }">Notícias</a>
+        <a href="../pages/loja.html" class="${
+          currentPath.endsWith("/pages/loja.html") ? "active" : ""
+        }">Loja</a>
+        <a href="../pages/contato.html" class="${
+          currentPath.endsWith("/pages/contato.html") ? "active" : ""
+        }">Contato</a>
+      </div>
     </div>
+    
+    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
     <script src="js/frontend.js"></script>
