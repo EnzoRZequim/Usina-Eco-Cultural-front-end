cfe0930544f7ee73a1eb93260dbc8bf849cc5ea7
Thamires <thamiipie@gmail.com>
2024-12-03 17:22:25 -0300

    refactor: noticias responsivas e melhor legibilidade

--- backend/noticias.js
+++ backend/noticias.js
@@ -5,45 +5,33 @@ document.addEventListener("DOMContentLoaded", () => {
     BuscarNoticias()
 });
 
-function BuscarNoticias(){
+function BuscarNoticias() {
     axios.get('http://localhost:3000/noticias')
-    .then(resposta =>{
+    .then(resposta => {
         const data = resposta.data;
 
         const container = document.getElementById('conteudo-noticia');
 
-        //limpar container
+        // Limpar container antes de adicionar novos itens
         container.innerHTML = '';
 
         data.forEach(item => {
-            //Criar Div:
             const div = document.createElement('div');
-                div.classList.add('body-caixas'); // Adiciona a classe para estilo
-                div.classList.add('col-5');
-
-            //Add Html à 
-            div.innerHTML =`
-                    <div class="caixa-evento">
-                        <div>
-                            <div class="d-flex">
-                                <div class="caixa-evento-titulo">
-                                    <h3>${item.titulo}</h3>
-                                </div>
-                            </div>
-                        
-                            <div class="caixa-mensagem m-4">
-                                <p>${item.info}</p>
-                            </div>                        
-                                </div>
-                            </div>
-                        </div>
+            div.classList.add('col-12', 'col-md-6', 'px-3'); // Para exibir duas notícias por linha em telas médias e grandes
+            
+            div.innerHTML = `
+                <div class="caixa-evento bg-white shadow mb-5 border rounded-5">
+                    <div class="d-flex flex-column flex-md-row pt-3 justify-content-between px-4">
+                        <h3 class="fw-bold text-start mb-3 mb-md-0 evento-info">${item.titulo}</h3>
                     </div>
-                `;
-
-                // Adiciona a div ao container principal
-                container.appendChild(div);
+                    <div class="caixa-mensagem m-4">
+                        <p>${item.info}</p>
+                    </div>
+                </div>
+            `;
+            
+            container.appendChild(div);
         });
     })
-    .catch(erro => console.log(erro))
-
+    .catch(erro => console.log(erro));
 }
