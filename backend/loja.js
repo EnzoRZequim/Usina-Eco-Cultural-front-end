165ce632c30a7c8d84c3dbe2f2668bab7f80a837
Thamires <thamiipie@gmail.com>
2024-12-04 16:27:55 -0300

    refactor: add imagem e descrições na loja

--- backend/loja.js
+++ backend/loja.js
@@ -5,49 +5,6 @@ document.addEventListener("DOMContentLoaded", () => {
     BuscarItem()
 });
 
-function BuscarItem(){
-    axios.get('http://localhost:3000/loja')
-    .then(resposta =>{
-        const data = resposta.data;
-
-        const container = document.getElementById('conteudo-loja');
-
-        //limpar container
-        container.innerHTML = '';
-
-        data.forEach(item => {
-            //Criar div
-            const div = document.createElement('div');
-                div.classList.add('body-caixas');
-                div.classList.add('col-5');
-
-            //add Html
-            div.innerHTML = div.innerHTML =`
-            <div class="caixa-evento">
-                <div>
-                    <div class="d-flex">
-                        <div class="caixa-evento-titulo">
-                            <h3>${item.nome}</h3>
-                        </div>
-                    </div>
-                
-                    <div class="caixa-mensagem m-4">
-                        <p>${item.preco}</p>
-                    </div>                        
-                        </div>
-                    </div>
-                </div>
-            </div>
-        `;
-
-        // Adiciona a div ao container principal
-        container.appendChild(div);
-});
-})
-.catch(erro => console.log(erro))
-
-}
-
 function adicionarAoCarrinho(nome, preco) {
     let carrinho = JSON.parse(localStorage.getItem('carrinho')) || [];
     carrinho.push({ nome, preco });
